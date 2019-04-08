9db97b9287809d6f8353ecf4b8db1f85492d76f4
oscar666666 <oscar88feichang6@gmail.com>
2019-04-07 21:35:24 -0600

    lab4 working

--- Lab4/LUdecomposition.py
+++ Lab4/LUdecomposition.py
@@ -26,21 +26,23 @@ class LuDecomposition:
         #temp = np.zeros((3, 3))
         temp=A
         t=0
+        t3=-1
         for i in range(len(A)-1):
 
             for row in range(t, len(A)):
 
                 t0 = temp[row][t] / temp[t][t]
-
+                t3 = t3+1
                 for column in range(len(A[0])):
                     if(row>t):
                         #print("row ", row, "---column", column, "temp",temp[row][t]/temp[t][t] )
                         #L[row][column] = temp[row][column] / temp[row-1][column]
                         if(temp[row][column] - ((t0)*temp[t][column])==0):
-                            L[row][column] = temp[row][column]/temp[row-1][column]
+                            if(L[row][column]==0):
+                                L[row][column] = temp[row][column]/temp[row-t3][column]
                         temp[row][column] = temp[row][column] - ((t0)*temp[t][column])
                 print("temp",temp)
+            t = t + 1
 
-            t=t+1
         return(L)
 
