bc638ec3ff4a95ce08d862cd15662c36adf3fc83
oscar666666 <oscar88feichang6@gmail.com>
2019-04-12 09:29:17 -0600

    right result

--- Lab5/main.py
+++ Lab5/main.py
@@ -2,14 +2,19 @@ from MultipleNewtonRaphson import *
 mnr = MNR()
 x = 1.5
 y = 3.5
-c = np.array([x, y])
 
-j = mnr.Jacobian(x, y)
-a = mnr.f1f2Matrix(x, y)
-z = mnr.MultiplyMatrix(mnr.CalculateInverse(j),a)
-f = mnr.SubtractMatrix(c, z)
-print(j)
-print(a)
-print(z)
-print("--------")
-print(f)
+while((abs(mnr.f1(x, y))+abs(mnr.f2(x,y)))>0.2):
+    c = np.array([x, y])
+    j = mnr.Jacobian(x, y)
+    a = mnr.f1f2Matrix(x, y)
+    z = mnr.MultiplyMatrix(mnr.CalculateInverse(j),a)
+    f = mnr.SubtractMatrix(c, z)
+    x = f[0]
+    y = f[1]
+    print(j)
+    print(a)
+    print(z)
+    print("--------")
+    print(f)
+    print(x)
+    print(y)
