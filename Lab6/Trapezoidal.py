05dd65df18c3a11b1780c37e404c9a7d39392824
oscar666666 <oscar88feichang6@gmail.com>
2019-04-19 07:08:43 -0600

    final modify

--- Lab6/Trapezoidal.py
+++ Lab6/Trapezoidal.py
@@ -4,6 +4,9 @@ class Trapezoidal:
     def f(self, x):
         return 2 - 5*x + 10*x**2 + 0.5*x**3
 
+    def Intergelf(self, x):
+        return 2*x -(5/2)*x**2 + (10/3)*x**3 + (0.5/4)*x**4
+
     def deltax(self, a, b, n):
         self.vdeltax = (b-a)/n
         return (b-a)/n
@@ -11,6 +14,9 @@ class Trapezoidal:
     def CalculateTrapezoidal(self, a, b, n):
         flow = self.f(a)
         fhigh = self.f(b)
-        for i in range(n-1):
-            self.deltaf = self.deltaf + self.f(a+(self.vdeltax*(i+1)))
+        for i in range(1,n):
+            self.deltaf = self.deltaf + self.f(a+(self.vdeltax*(i)))
         return (self.vdeltax/2)*(flow+(2*self.deltaf)+fhigh)
+    def zero(self):
+        self.vdeltax = 0
+        self.deltaf = 0
--- Lab6/main.py
+++ Lab6/main.py
@@ -2,12 +2,29 @@ import  matplotlib.pyplot as plt
 import numpy as np
 from Trapezoidal import *
 tp = Trapezoidal()
-x = np.arange(-10, 10, 0.01)
-plt.plot(x, tp.f(x))
-plt.xlabel('x')
-plt.ylabel('f(x)')
+# x = np.arange(-10, 10, 0.01)
+# plt.plot(x, tp.f(x))
+# plt.xlabel('x')
+# plt.ylabel('f(x)')
+# plt.title('Trapezoidal')
+# plt.savefig("f(x).png")
+n = int(input("What is n? "))
+print("delta x", tp.deltax(-10, 10, n))
+print("Trapezoidal value", tp.CalculateTrapezoidal(-10, 10, n))
+print("True Intergel value", tp.Intergelf(10)-tp.Intergelf(-10))
+print("true percentage error",(((tp.Intergelf(10) - tp.Intergelf(-10)) - tp.CalculateTrapezoidal(-10, 10, n)) / (tp.Intergelf(10) - tp.Intergelf(-10))))
+x = np.arange(1,10,1)
+a = []
+for i in range(1,10):
+    tp.zero()
+    tp.deltax(-10, 10, i)
+    a.append((((tp.Intergelf(10) - tp.Intergelf(-10)) - tp.CalculateTrapezoidal(-10, 10, i)) / ((tp.Intergelf(10) - tp.Intergelf(-10)))))
+#plt.plot(y, ((tp.Intergelf(10)-tp.Intergelf(-10)) - tp.CalculateTrapezoidal(-10, 10, a))/(tp.Intergelf(10)-tp.Intergelf(-10)))
+plt.plot(x, a)
+plt.xlabel('n')
+plt.ylabel('True percentage relative error in %')
 plt.title('Trapezoidal')
-plt.savefig("f(x).png")
-print(tp.deltax(-10, 10, 6))
-print(tp.CalculateTrapezoidal(-10, 10, 6))
+plt.savefig("true percentage relative error.png")
 plt.show()
+
+
